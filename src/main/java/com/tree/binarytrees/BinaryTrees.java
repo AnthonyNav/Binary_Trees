0427115dcfbdc9d2b05c5108659d85daa61329ea
Anthony <anthonydevxp@gmail.com>
2024-04-28 18:33:27 -0600

    project base

---
+++ src/main/java/com/tree/binarytrees/BinaryTrees.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tree.binarytrees;
+
+/**
+ *
+ * @author antho
+ */
+public class BinaryTrees {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
